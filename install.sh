bffc4ee7ef562680d01cfc21bcbfe00a26fa4de4
Matthew Alvarez <matt@mattjalvarez.com>
2020-11-25 15:12:47 -0600

    some tweaks

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@ set -e
 
 ACTION="$1"
 
-helm repo add jupyterhub https://jupyterhub.github.io/helm-chart/
-helm repo add minio https://helm.min.io/
-helm repo add bitnami https://charts.bitnami.com/bitnami
-helm repo add apache https://pulsar.apache.org/charts
-helm repo add stable https://kubernetes-charts.storage.googleapis.com
+# helm repo add jupyterhub https://jupyterhub.github.io/helm-chart/
+# helm repo add minio https://helm.min.io/
+# helm repo add bitnami https://charts.bitnami.com/bitnami
+# helm repo add apache https://pulsar.apache.org/charts
+# helm repo add stable https://kubernetes-charts.storage.googleapis.com
 
 helm repo update
 
@@ -30,5 +30,5 @@ cd ../
 # helm $ACTION refit ./refit -f envs/local/values.yaml
 
 # To install on tiger 
-# helm install refit ./refit -f envs/gcp/values.yaml
-helm upgrade refit ./refit --namespace refit-f envs/tiger/values.yaml
+helm install refit ./refit --namespace refit -f envs/tiger/values.yaml
+# helm upgrade refit ./refit --namespace refit -f envs/tiger/values.yaml
