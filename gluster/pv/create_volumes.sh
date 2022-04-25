5ecd49ad11eee7812d33c3690bfe37adaa9e1654
Tony Luo <tonyluozn@gmail.com>
2022-04-25 17:19:45 -0500

    Update namespace/dependency/PV
     - Create two namespaces
     - Update dependencies
     - Update persistent volumes

--- gluster/pv/create_volumes.sh
+++ gluster/pv/create_volumes.sh
@@ -2,7 +2,7 @@
 
 for i in {1..50}
 do
-    echo "Creating folder /mnt-gluster/cdl-data/refit/v2/vol-$i"
-    mkdir /mnt-gluster/cdl-data/refit/v2/vol-$i
-    chmod -R 777 /mnt-gluster/cdl-data/refit/v2/vol-$i
+    echo "Creating folder /mnt-gluster/cdl-data/refit/vol5/vol-$i"
+    mkdir /mnt-gluster/cdl-data/refit/vol5/vol-$i
+    chmod -R 777 /mnt-gluster/cdl-data/refit/vol5/vol-$i
 done
